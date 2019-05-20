71f7213fb34b382b9e1561a3d92a7dd6a8a5b4ce
Nguyễn Duy Long <duylong3110@gmail.com>
2019-05-20 11:49:53 +0700

    edit modal

--- src/resources/views/vendor/backpack/modal/buttons/create.blade.php
+++
@@ -1,3 +0,0 @@
-@if ($crud->hasAccess('create'))
-    <a data-route ="{{ url($crud->route.'/create') }}" data-toggle="modal" class="btn btn-primary ladda-button" data-style="zoom-in" data-target="#backpackModal" id="modal" data-id =""><span class="ladda-label"><i class="fa fa-plus"></i> {{ trans('backpack::crud.add') }} {{ $crud->entity_name }}</span></a>
-@endif
--- src/resources/views/vendor/backpack/modal/buttons/preview.blade.php
+++
@@ -1,5 +0,0 @@
-{{-- This button is deprecated and will be removed in CRUD 3.5 --}}
-
-@if ($crud->hasAccess('show'))
-    <a data-route ="{{ url($crud->route.'/'.$entry->getKey()) }}" data-toggle="modal" class="btn btn-xs btn-default" data-target="#backpackModal" id="modal"><i class="fa fa-eye"></i> {{ trans('backpack::crud.preview') }}</a>
-@endif
